b2dd9e3dbe454df725383288f0d43531e9dfc75f
ViRT1ST <ViRT1ST@outlook.com>
2024-08-30 13:55:52 +0500

    30-08-2024_03

--- src/components/debug/whoami-rsc.tsx
+++ src/components/debug/whoami-rsc.tsx
@@ -1,12 +1,15 @@
 import { headers } from 'next/headers';
 
-export default async function WhoAmIRSC() {
-  const { user } = await fetch('http://localhost:3000/api/debug', {
-    method: 'GET',
-    // headers: headers(),
-  }).then((res) => res.json());
+import { ROOT_PATH } from '@/constants';
 
-  return (
-    <div>Who Am I (RSC): {user?.name}</div>
-  );
+export default async function WhoAmIRSC() {
+  // const { user } = await fetch(`${ROOT_PATH}/api/debug`, {
+  //   method: 'GET',
+  //   headers: headers(),
+  // }).then((res) => res.json());
+
+  // return (
+  //   <div>Who Am I (RSC): {user?.name}</div>
+  // );
+  return <div>Empty</div>;
 }
