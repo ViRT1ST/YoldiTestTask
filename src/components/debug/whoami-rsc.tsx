8dca98a79fd847aac8794f19d79e64a1849fdc47
ViRT1ST <ViRT1ST@outlook.com>
2024-08-30 13:12:44 +0500

    30-08-2024_02

--- src/components/debug/whoami-rsc.tsx
+++ src/components/debug/whoami-rsc.tsx
@@ -3,7 +3,7 @@ import { headers } from 'next/headers';
 export default async function WhoAmIRSC() {
   const { user } = await fetch('http://localhost:3000/api/debug', {
     method: 'GET',
-    headers: headers(),
+    // headers: headers(),
   }).then((res) => res.json());
 
   return (
