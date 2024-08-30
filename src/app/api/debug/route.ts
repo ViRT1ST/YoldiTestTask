aa6549b3521d34e7b5d4e9140a34844896df4522
ViRT1ST <ViRT1ST@outlook.com>
2024-08-30 13:57:11 +0500

    30-08-2024_04

--- src/app/api/debug/route.ts
+++ src/app/api/debug/route.ts
@@ -2,10 +2,5 @@ import { NextResponse } from 'next/server';
 import { auth } from '@/lib/auth/next-auth';
 
 export const GET = auth(async ({ auth }) => {
-  // const session = await auth() as SessionWithBaseData;
-  // const sessionUser = session?.user;
-  // const sessionUuid = sessionUser?.uuid;
-  console.log('ddd', auth?.user)
-
   return NextResponse.json({ user: auth?.user });
 });
